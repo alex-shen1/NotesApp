e5bfd21c2b387d7a0e7de9e42fd564a62732fa02
Alex Shen <as5gd@viginia.edu>
2020-05-15 23:29:36 -0400

    Updates

---
+++ precache-manifest.41764252bdd70deef37574e861199811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1064ca9771be78ed4bb66276b0efe2",
+    "url": "/LaunchNotes/index.html"
+  },
+  {
+    "revision": "f4031a33cddb9472ac22",
+    "url": "/LaunchNotes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "adfc38e6f96b3a817e9d",
+    "url": "/LaunchNotes/static/js/2.071e8f0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LaunchNotes/static/js/2.071e8f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4031a33cddb9472ac22",
+    "url": "/LaunchNotes/static/js/main.5b9bad10.chunk.js"
+  },
+  {
+    "revision": "d326b8a43991a5345966",
+    "url": "/LaunchNotes/static/js/runtime-main.0c943f27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LaunchNotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
