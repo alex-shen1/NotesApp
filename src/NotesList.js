f9f80801729ae83e513eb90026dc3590e433baa0
annie-sharkey <sharkeyconwaya@gmail.com>
2020-05-18 14:09:05 -0400

    Comments NoteList.js

--- src/NotesList.js
+++ src/NotesList.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+
+/*This would be a great place to use a functional component for when we don't manage state within a component.
+I would write this like the following:
+const NotesList = ({deleteNoteFunc, setEditingNoteFunc, etc.}) => {
+    return ()
+    
+  }
+  
+  change settings to auto format on save
+  
+  On naming conventions: don't need to note that a variable is a function or a list or a string. 
+  i.e. you can remove "Func" from your function names here
+    
+    */
 class NotesList extends Component {
     render() {
         return <div id="notesList">
